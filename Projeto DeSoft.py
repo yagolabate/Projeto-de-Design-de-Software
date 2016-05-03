8ae95fd0082a4f326f9dc77fb8d4d6f3159a2858
yagolabate <yagolabate8@hotmail.com>
2016-05-03 13:43:30 -0300

    reajustei a posição dos botões na frame do henrique

--- Projeto DeSoft.py
+++ Projeto DeSoft.py
@@ -50,14 +50,14 @@ class Projeto_Final:
         self.label = tk.Label(self.pagina1,image=self.load_emagrecer)
         self.label.grid(row=2,column=1,sticky='nsew')  
         
-        self.botao_massa = tk.Button(self.pagina1, width=35)
+        self.botao_massa = ttk.Button(self.pagina1, width=35)
         self.botao_massa.grid(row=3,column=0,sticky='ns')
-        self.botao_massa.configure(text='Ganhar Massa', font = 50)
+        self.botao_massa.configure(text='Ganhar Massa')
         self.botao_massa.configure(command=self.clicar_GanharMassa)
         
-        self.botao_emagrecer = tk.Button(self.pagina1, width=35)
+        self.botao_emagrecer = ttk.Button(self.pagina1, width=35)
         self.botao_emagrecer.grid(row=3,column=1,sticky='ns')
-        self.botao_emagrecer.configure(text='Emagrecer', font = 50)
+        self.botao_emagrecer.configure(text='Emagrecer')
         self.botao_emagrecer.configure(command=self.clicar_Emagrecer)
         
         
@@ -125,59 +125,59 @@ class Projeto_Final:
         
         
         self.lfrase = tk.Label(self.pagina2)
-        self.lfrase.grid(row=2,column=2,columnspan=4, sticky='nsew')
+        self.lfrase.grid(row=2,column=2,columnspan=3, sticky='nsew')
         self.lfrase.configure(fg="black", text="Selecione seu nivel de atividade fisica:")
         
         
         self.mostrar1 = Image.open('sedentarismo.jpg').resize((150,100))
         self.plotar1 = ImageTk.PhotoImage(self.mostrar1)
         self.imagem1 = ttk.Button(self.pagina2,image = self.plotar1)
-        self.imagem1.grid(row = 3, column = 0, sticky = 'nsew')
+        self.imagem1.grid(row = 3, column = 1, sticky = 'nsew')
         
         
         self.mostrar2 = Image.open('levemente.jpg').resize((150,100))
         self.plotar2 = ImageTk.PhotoImage(self.mostrar2)
         self.imagem2 = ttk.Button(self.pagina2, image = self.plotar2)
-        self.imagem2.grid(row = 3, column = 2, sticky = 'nsew')
+        self.imagem2.grid(row = 3, column = 3, sticky = 'nsew')
         
         
         self.mostrar3 = Image.open('ativo.jpg').resize((150,100))
         self.plotar3 = ImageTk.PhotoImage(self.mostrar3)        
         self.imagem3 = ttk.Button(self.pagina2, image = self.plotar3)
-        self.imagem3.grid(row = 3, column = 6, sticky = 'nsew')
+        self.imagem3.grid(row = 3, column = 5, sticky = 'nsew')
         
         self.mostrar4 = Image.open('bastante ativo.jpg').resize((150,100))
         self.plotar4 = ImageTk.PhotoImage(self.mostrar4)
         self.imagem4 = ttk.Button(self.pagina2, image = self.plotar4)
-        self.imagem4.grid(row = 5, column = 0, sticky = 'nsew')
+        self.imagem4.grid(row = 5, column = 1, sticky = 'nsew')
         
         self.mostrar5 = Image.open('muito ativo.jpg').resize((150,100))
         self.plotar5 = ImageTk.PhotoImage(self.mostrar5)
         self.imagem5 = ttk.Button(self.pagina2, image = self.plotar5)
-        self.imagem5.grid(row = 5, column =2, sticky = 'nsew')
+        self.imagem5.grid(row = 5, column =3, sticky = 'nsew')
     
         self.legenda1 = tk.Label(self.pagina2)
-        self.legenda1.grid(row = 4, column = 0, sticky = 'nsew')
+        self.legenda1.grid(row = 4, column = 1, sticky = 'nsew')
         self.legenda1.configure(fg = 'black', text = 'sedentário')
         
         self.legenda2 = tk.Label(self.pagina2)
-        self.legenda2.grid(row = 4, column = 2, sticky = 'nsew')
+        self.legenda2.grid(row = 4, column = 3, sticky = 'nsew')
         self.legenda2.configure(fg = 'black', text = 'levemente ativo')
         
         self.legenda3 = tk.Label(self.pagina2)
-        self.legenda3.grid(row = 4, column = 6, sticky = 'nsew')
+        self.legenda3.grid(row = 4, column = 5, sticky = 'nsew')
         self.legenda3.configure(fg = 'black', text = 'moderamente ativo')
         
         self.legenda4 = tk.Label(self.pagina2)
-        self.legenda4.grid(row = 6, column = 0, sticky = 'nsew')
+        self.legenda4.grid(row = 6, column = 1, sticky = 'nsew')
         self.legenda4.configure(fg = 'black', text = 'muito ativo')
         
         self.legenda5 = tk.Label(self.pagina2)
-        self.legenda5.grid(row = 6, column = 2, sticky = 'nsew')
+        self.legenda5.grid(row = 6, column = 3, sticky = 'nsew')
         self.legenda5.configure(fg = 'black', text = 'extremamente ativo')
         
         self.botao_voltar_pag1 = ttk.Button(self.pagina2,width=20)
-        self.botao_voltar_pag1.grid(row=6,column=6,sticky='se')
+        self.botao_voltar_pag1.grid(row=6,column=5,sticky='se')
         self.botao_voltar_pag1.configure(text="Voltar")
         self.botao_voltar_pag1.configure(command=self.pagina1.tkraise)
         
